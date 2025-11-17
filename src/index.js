577784121435b392e197a7e2fb86ad3bdc60491b
Vladimir-cloudj <wizart88@gmail.com>
2025-11-17 21:31:00 +0400

    feat: work with db firebase

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import cart from './modules/cart';
+import getData from './modules/getData';
+import second from './modules/second';
 
+getData();
+// getData().then((data) => console.log(data));
 cart();
+second();
---
+++ src/modules/getData.js
@@ -0,0 +1,18 @@
+const getData = () => {
+    // return fetch("https://jsonplaceholder.typicode.com/posts", {
+    //   method: "POST",
+    //   body: JSON.stringify({
+    //     title: "foo",
+    //     body: "bar",
+    //     userId: 1,
+    //   }),
+    //   headers: {
+    //     "Content-type": "application/json; charset=UTF-8",
+    //   },
+    // })
+    return fetch(
+      "https://ozon-d3bcc-default-rtdb.firebaseio.com/goods.json"
+    ).then((res) => res.json());
+}
+
+export default getData
