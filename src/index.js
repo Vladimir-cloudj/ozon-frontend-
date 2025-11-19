0ca004e53df743f378b28a27c2eb84636b6eaa37
Vladimir-cloudj <wizart88@gmail.com>
2025-11-19 14:44:47 +0400

    feat: функция фильтрации по цене, фильтрация по критерию - акция

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import getData from './modules/getData';
 import load from './modules/load';
 import search from './modules/search';
 import catalog from './modules/catalog';
+import filter from './modules/filter';
 
 // getData();
 // getData().then((data) => console.log(data));
@@ -10,3 +11,4 @@ cart();
 load();
 search();
 catalog();
+filter();
